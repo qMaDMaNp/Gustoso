a181a65066182594432932b6bb02f928281a66b4
qMaDMaNp <medifman@gmail.com>
2018-04-06 13:01:58 +0300

    gulp set

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 'use strict'
 const gulp = require('gulp');
 const postcss = require('gulp-postcss');
-const cssnano = require('cssnano');
+const rename = require('gulp-rename');
 
-gulp.task('autoprefixer', function () {
-  var processors = [
-    cssnano()
-  ];
-    return gulp.src('./src/style/*.css')
-        .pipe(postcss(processors))
-        .pipe(gulp.dest('./dest'));
+gulp.task('css', function() {
+    return gulp.src('./src/style/*.pcss')
+        .pipe(postcss([
+          require('postcss-cssnext')({autoprefixer: false}),
+          require('postcss-assets')({
+            loadPaths: ['src/img/'],
+            relativeTo: 'src/style/'
+          })
+        ]))
+        .pipe(rename('main.css'))
+        .pipe(gulp.dest('./src/style/'));
+});
+
+gulp.task('default', function() {
+    gulp.watch('./src/style/*.pcss', ['css'])
 });
