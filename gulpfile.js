e6fb0f356354d039ea52fdd7765efd4edd3fe2f3
unknown <medifman@gmail.com>
2018-04-07 00:00:58 +0300

    v1.2

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,25 @@ const gulp = require('gulp');
 const postcss = require('gulp-postcss');
 const rename = require('gulp-rename');
 const browserSync = require('browser-sync').create();
+const precss = require('precss');
 
 gulp.task('css', function() {
-    return gulp.src('./src/style/*.pcss')
-        .pipe(postcss([
-          require('postcss-cssnext')({autoprefixer: false}),
-          require('postcss-assets')({
-            loadPaths: ['src/img/'],
-            relativeTo: 'src/style/'
-          })
-        ]))
-        .pipe(rename('main.css'))
-        .pipe(gulp.dest('./src/style/'))
-        .pipe(browserSync.reload({
-          stream: true
-        }))
+  return gulp.src('./src/style/*.pcss')
+    .pipe(postcss([
+      require('postcss-cssnext')({
+        autoprefixer: false
+      }),
+      require('postcss-assets')({
+        loadPaths: ['src/img/'],
+        relativeTo: 'src/style/'
+      }),
+      require('precss')
+    ]))
+    .pipe(rename('main.css'))
+    .pipe(gulp.dest('./src/style/'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
 gulp.task('browserSync', function() {
@@ -29,7 +33,7 @@ gulp.task('browserSync', function() {
 });
 
 gulp.task('default', ['browserSync', 'css'], function() {
-    gulp.watch('src/*.html', browserSync.reload);
-    gulp.watch('src/js/**/*.js', browserSync.reload);
-    gulp.watch('./src/style/*.pcss', ['css'])
+  gulp.watch('src/*.html', browserSync.reload);
+  gulp.watch('src/js/**/*.js', browserSync.reload);
+  gulp.watch('./src/style/*.pcss', ['css'])
 });
