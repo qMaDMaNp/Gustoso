52161ee63a709ad6bd20ee90d916f781a554b231
qMaDMaNp <medifman@gmail.com>
2018-04-06 11:16:40 +0300

    22

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,10 @@ const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
 
 gulp.task('autoprefixer', function () {
-<<<<<<< HEAD
-    return gulp.src('./src/style/*.css')
-        .pipe(sourcemaps.init())
-        .pipe(postcss([ autoprefixer() ]))
-        .pipe(sourcemaps.write('.'))
-=======
   var processors = [
     cssnano()
   ];
     return gulp.src('./src/style/*.css')
         .pipe(postcss(processors))
->>>>>>> 8382896eea4ff808971f560fb9a0213799d11082
         .pipe(gulp.dest('./dest'));
 });
