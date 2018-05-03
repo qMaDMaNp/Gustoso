9a6f64d39a99089961c436ff8e2e6d934b7b41ef
unknown <medifman@gmail.com>
2018-05-03 17:28:26 +0300

    slider

---
+++ src/js/dish-slider.js
@@ -0,0 +1,72 @@
+const dishesSliderListElem = document.querySelector(".dishes-slider-list");
+const dishesLeftWrapElem = document.querySelector(".dishes-left-wrap");
+const dishesSliderBigImgElem = document.querySelector(".dishes-left-wrap__img");
+const leftArrowElem = document.querySelector(".dishes-slider__btn-left");
+const rightArrowElem = document.querySelector(".dishes-slider__btn-right");
+let valueCounter = '3';
+
+dishesSliderListElem.addEventListener('click', listItemClicked, false);
+leftArrowElem.addEventListener('click', arrowClicked, false);
+rightArrowElem.addEventListener('click', arrowClicked, false);
+
+function arrowClicked(event) {
+  if (event.currentTarget === leftArrowElem) {
+    switch (valueCounter) {
+      case "1":
+        valueCounter = "3";
+        animateSlider(valueCounter);
+        break;
+      case "2":
+        animateSlider((valueCounter - 1).toString());
+        break;
+      case "3":
+        animateSlider((valueCounter - 1).toString());
+        break;
+    }
+  } else if (event.currentTarget === rightArrowElem) {
+    switch (valueCounter) {
+      case "1":
+        animateSlider((parseInt(valueCounter) + 1).toString());
+        break;
+      case "2":
+        animateSlider((parseInt(valueCounter) + 1).toString());
+        break;
+      case "3":
+        valueCounter = "1";
+        animateSlider(valueCounter);
+        break;
+    }
+  }
+};
+
+function listItemClicked(event) {
+  let targetValue = event.target.getAttribute("val");
+  switch (targetValue) {
+    case "1":
+      animateSlider(targetValue);
+      break;
+    case "2":
+      animateSlider(targetValue);
+      break;
+    case "3":
+      animateSlider(targetValue);
+      break;
+  };
+};
+
+function animateSlider(target) {
+  //add value
+  valueCounter = target;
+  //clear previous state
+  dishesLeftWrapElem.classList.remove('animation');
+  dishesLeftWrapElem.classList.remove('animation-reverse');
+  //start event
+  dishesLeftWrapElem.classList.add('animation');
+  //check if transition ended
+  dishesLeftWrapElem.addEventListener("transitionend", function() {
+    //change img
+    dishesSliderBigImgElem.src = 'img/big_img' + target + '.png';
+    //start event reverse
+    dishesLeftWrapElem.classList.add('animation-reverse');
+  });
+}
